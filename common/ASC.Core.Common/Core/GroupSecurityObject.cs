0ed487379cd23e39922f43e69a91743ec618b55c
MaksimChegulov <maksim.chegulov@onlyoffice.com>
2021-11-15 19:36:19 +0300

    add group security object

---
+++ common/ASC.Core.Common/Core/GroupSecurityObject.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ASC.Common.Security;
+using ASC.Common.Security.Authorizing;
+
+namespace ASC.Core
+{
+    public class GroupSecurityObject : ISecurityObject
+    {
+        public object SecurityId { get; set; }
+
+        public Type ObjectType { get; set; }
+
+        public bool InheritSupported => false;
+
+        public bool ObjectRolesSupported => false;
+
+        public GroupSecurityObject(Guid groupId)
+        {
+            SecurityId = groupId;
+            ObjectType = typeof(Guid);
+        }
+
+        public IEnumerable<IRole> GetObjectRoles(ISubject account, ISecurityObjectId objectId, SecurityCallContext callContext)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ISecurityObjectId InheritFrom(ISecurityObjectId objectId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
