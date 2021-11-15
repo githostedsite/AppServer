ec1a232c7539291d6940b2488dbd19e0e5cdd0e7
MaksimChegulov <maksim.chegulov@onlyoffice.com>
2021-11-15 19:41:14 +0300

    add model

---
+++ products/ASC.Files/Core/Model/UserModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASC.Files.Core.Model
+{
+    public class UserModel
+    {
+        public Guid UserId { get; set; }
+    }
+}
