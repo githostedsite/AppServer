58c3e20e5ecb9e87cc45ad6689eb0fa2b6a0c059
MaksimChegulov <maksim.chegulov@onlyoffice.com>
2021-11-17 20:42:16 +0300

    Files: add modes

---
+++ products/ASC.Files/Core/Model/CreateRoomModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ASC.Files.Core.Model
+{
+    public class CreateRoomModel
+    {
+        public string Title { get; set; }
+        public bool Privacy { get; set; }
+    }
+}
---
+++ products/ASC.Files/Core/Model/UsersModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ASC.Files.Core.Model
+{
+    public class UsersModel
+    {
+        public IEnumerable<Guid> UsersIds { get; set; }
+    }
+}
