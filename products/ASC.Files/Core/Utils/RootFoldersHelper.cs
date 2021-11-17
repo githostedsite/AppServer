7268996b9cc7ecfb481408db16278711cae0970d
MaksimChegulov <maksim.chegulov@onlyoffice.com>
2021-11-17 18:30:49 +0300

    Files: add root folder helper

---
+++ products/ASC.Files/Core/Utils/RootFoldersHelper.cs
@@ -0,0 +1,12 @@
+﻿namespace ASC.Files.Core.Utils
+{
+    public static class RootFoldersHelper
+    {
+        public static string GetBunchKey(FolderType type, string data, string module = "files") =>
+            type switch
+            {
+                FolderType.Custom => $"{module}/custom/{data}",
+                FolderType.CustomPrivacy => $"{module}/customprivacy/{data}"
+            };
+    }
+}
