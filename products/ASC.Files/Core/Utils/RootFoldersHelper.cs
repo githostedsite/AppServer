330a8c901839ec9fd2b3d0bd4812e3b9840acd3d
MaksimChegulov <maksim.chegulov@onlyoffice.com>
2021-11-22 15:05:34 +0300

    Files: add root folders cases

--- products/ASC.Files/Core/Utils/RootFoldersHelper.cs
+++ products/ASC.Files/Core/Utils/RootFoldersHelper.cs
@@ -6,7 +6,16 @@
             type switch
             {
                 FolderType.Custom => $"{module}/custom/{data}",
-                FolderType.CustomPrivacy => $"{module}/customprivacy/{data}"
+                FolderType.Privacy => $"{module}/privacy/{data}",
+                FolderType.CustomPrivacy => $"{module}/customprivacy/{data}",
+                FolderType.COMMON => $"{module}/common/{data}",
+                FolderType.BUNCH => $"{module}/bunch/{data}",
+                FolderType.TRASH => $"{module}/trash/{data}",
+                FolderType.SHARE => $"{module}/share/{data}",
+                FolderType.Projects => $"{module}/projects/{data}",
+                FolderType.Favorites => $"{module}/favorites/{data}",
+                FolderType.Recent => $"{module}/recent/{data}",
+                FolderType.Templates => $"{module}/templates/{data}"
             };
     }
 }
