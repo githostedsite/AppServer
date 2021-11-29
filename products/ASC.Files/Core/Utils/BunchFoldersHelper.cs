ad035ab97d01babd5af0f7bdf0b5d80737c1dca6
MaksimChegulov <maksim.chegulov@onlyoffice.com>
2021-11-29 13:30:42 +0300

    Files: add bunch helper

---
+++ products/ASC.Files/Core/Utils/BunchFoldersHelper.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace ASC.Files.Core.Utils
+{
+    public static class BunchFoldersHelper
+    {
+        public static string GetBunchKey(FolderType type, string data, string module = "files") =>
+            type switch
+            {
+                FolderType.Custom => $"{module}/custom/{data}",
+                FolderType.Privacy => $"{module}/privacy/{data}",
+                FolderType.CustomPrivacy => $"{module}/customprivacy/{data}",
+                FolderType.COMMON => $"{module}/common/{data}",
+                FolderType.BUNCH => $"{module}/bunch/{data}",
+                FolderType.TRASH => $"{module}/trash/{data}",
+                FolderType.SHARE => $"{module}/share/{data}",
+                FolderType.Projects => $"{module}/projects/{data}",
+                FolderType.Favorites => $"{module}/favorites/{data}",
+                FolderType.Recent => $"{module}/recent/{data}",
+                FolderType.Templates => $"{module}/templates/{data}"
+            };
+
+        public static string GetProvider(string rootFolderId)
+        {
+            return rootFolderId.Split('-').FirstOrDefault();
+        }
+
+        public static string GetEntryId(string folderId)
+        {
+            return Regex.Replace(folderId, @"^\S+-\d+-\|", "", RegexOptions.Compiled);
+        }
+
+        public static string MakeFolderId(string entryId, string provider, int providerId)
+        {
+            return $"{provider}-{providerId}-|{entryId}";
+        }
+    }
+}
