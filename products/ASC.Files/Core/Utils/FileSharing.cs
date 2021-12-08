43318da44ee6d3251376af5840ef714474d47a48
MaksimChegulov <maksim.chegulov@onlyoffice.com>
2021-12-08 18:44:53 +0300

    Files: add rule for archive folder

--- products/ASC.Files/Core/Utils/FileSharing.cs
+++ products/ASC.Files/Core/Utils/FileSharing.cs
@@ -249,6 +249,7 @@ namespace ASC.Web.Files.Utils
                 entry != null
                 && ((entry.RootFolderType == FolderType.COMMON && Global.IsAdministrator) 
                 || (entry.RootFolderType == FolderType.VirtualRoom && (Global.IsAdministrator || FileSecurity.CanEdit(entry)))
+                    || (entry.RootFolderType == FolderType.Archive && Global.IsAdministrator)
                     || !UserManager.GetUsers(AuthContext.CurrentAccount.ID).IsVisitor(UserManager)
                         && (entry.RootFolderType == FolderType.USER
                             && (Equals(entry.RootFolderId, GlobalFolderHelper.FolderMy) || FileSecurity.CanEdit(entry))
