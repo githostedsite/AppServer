fb98da69dc68d11d50540a458ce9571383dedc27
MaksimChegulov <maksim.chegulov@onlyoffice.com>
2022-01-10 17:53:08 +0300

    Web: add logo resize worker

---
+++ web/ASC.Web.Core/Files/LogoResizeWorkerItem.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+using ASC.Common;
+using ASC.Data.Storage;
+using ASC.Web.Core.Images;
+
+using SixLabors.ImageSharp;
+
+namespace ASC.Web.Core.Files
+{
+    [Transient]
+    public class LogoResizeWorkerItem<T> : ResizeWorkerItem
+    {
+        public T FolderId { get; }
+        public RoomLogoSettings Settings { get; }
+
+        public LogoResizeWorkerItem() { }
+
+        public LogoResizeWorkerItem(T folderId, byte[] data, long maxFileSize, Size size, IDataStore dataStore, RoomLogoSettings settings)
+            : base(data, maxFileSize, size, dataStore)
+        {
+            FolderId = folderId;
+            Settings = settings;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public bool Equals(LogoResizeWorkerItem<T> other)
+        {
+            if (other is null) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return other.FolderId.Equals(FolderId) && other.MaxFileSize == MaxFileSize && other.Size.Equals(Size);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(FolderId, MaxFileSize, Size);
+        }
+    }
+}
