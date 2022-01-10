6ad9deb9276d85bd1a2fe20bbd7fd8c7e6a49ab7
MaksimChegulov <maksim.chegulov@onlyoffice.com>
2022-01-10 17:43:59 +0300

    Core: add ImageSettings

---
+++ common/ASC.Core.Common/Settings/IImageSettings.cs
@@ -0,0 +1,11 @@
+﻿using SixLabors.ImageSharp;
+
+namespace ASC.Core.Common.Settings
+{
+    public interface IImageSettings : ISettings
+    {
+        public Point Point { get; set; }
+        public Size Size { get; set; }
+        public bool IsDefault { get; set; }
+    }
+}
